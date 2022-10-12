723ffcb7fcb4986490c2a8dce2275dbd6fdc688f
Ergashgbaeva <farangizergashbaeva24@gmail.com>
2022-10-12 19:05:45 +0500

    blabla

--- script.js
+++ script.js
@@ -54,3 +54,19 @@ function uniqueInOrder(it) {
   
   return result
 }
+
+// seventh
+
+var uniqueInOrder = function(iterable) {
+  var arr = [];
+  for (var i=0;i<iterable.length;i++) {
+
+  // check if there is back to back letter
+  if (iterable[i] !== iterable[i+1]) {
+
+      // if not, push the current value to arr
+    arr.push(iterable[i]);
+  }
+}
+return arr;
+}
