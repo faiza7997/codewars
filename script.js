ba03037a5428a6aa2922afbb45d5398f3c3d314e
Ergashgbaeva <farangizergashbaeva24@gmail.com>
2022-10-14 15:08:10 +0500

    nine

--- script.js
+++ script.js
@@ -85,3 +85,21 @@ function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
 function countSheeps(arrayOfSheeps) {
   return arrayOfSheeps.filter(Boolean).length;
 }
+
+// ten 
+
+function showObjectKeys(obj){
+  for (var key in obj){
+    console.log(key);
+  }
+}
+function showObjectValues(obj){
+  for (var key in obj){
+    console.log(obj[key]);
+  }
+}
+var ob={item1:"This",item2:"is",item3:"an",item4:"example"};
+console.log("keys of ob:")
+showObjectKeys(ob);
+console.log("values of ob:")
+showObjectValues(ob);
