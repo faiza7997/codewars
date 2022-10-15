627184b649b735e8514fe6bb4c06b0e1b8a18fea
Ergashgbaeva <farangizergashbaeva24@gmail.com>
2022-10-15 21:00:50 +0500

    twelve

--- script.js
+++ script.js
@@ -114,3 +114,17 @@ function giveMeFive(obj){
   }
   return five;
 }
+
+// twelve
+
+function pickIt (arr) {
+  
+  let odd = []
+  let even =[]
+      
+  for (var x of arr) {
+      ((x % 2) ? odd : even).push(x)    
+  }
+      
+  return [odd, even]
+}
