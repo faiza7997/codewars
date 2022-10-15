a96a7901b4bf509d04cfd42fb301b313e803506f
Ergashgbaeva <farangizergashbaeva24@gmail.com>
2022-10-15 21:02:48 +0500

    thirteen

--- script.js
+++ script.js
@@ -128,3 +128,11 @@ function pickIt (arr) {
       
   return [odd, even]
 }
+
+// thirteen
+
+function sortDict(dict){
+  return Object.keys(dict)
+    .map(function(v){ return [+v || v, dict[v]] })
+    .sort(function(a, b){ return a[1] < b[1] });
+}
