f4946b84150dd3abb31ad4fc4b1fd3386955f594
Ergashgbaeva <farangizergashbaeva24@gmail.com>
2022-10-18 12:46:37 +0500

    fourteen

--- script.js
+++ script.js
@@ -146,3 +146,13 @@ function powersOfTwo(n){
   }
   return result;
 }
+
+
+//  fiveteen 
+
+
+function sortDict(dict){
+  return Object.keys(dict)
+    .map(function(v){ return [+v || v, dict[v]] })
+    .sort(function(a, b){ return a[1] < b[1] });
+}
