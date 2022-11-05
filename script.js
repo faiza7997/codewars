40c05a188c534dce2298f5b604a249cfce251bed
Farangiz <109240720+Ergashgbaeva@users.noreply.github.com>
2022-11-05 15:33:39 +0500

    8kyu

--- script.js
+++ script.js
@@ -232,6 +232,8 @@ reverse = function(array) {
   }
 
 
-  const isPalindrome = (x) => {
-    return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
-  }
+  // const isPalindrome = (x) => {
+  //   return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
+  // }
+
+  const isPalindrome = x => x.toLowerCase().split('').reverse().join('') == x.toLowerCase();
