e1798e971a307df3b18bc713d1266640962fec47
Farangiz <109240720+Ergashgbaeva@users.noreply.github.com>
2022-11-07 17:42:25 +0500

    8kyu

--- script.js
+++ script.js
@@ -242,3 +242,6 @@ reverse = function(array) {
   const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     return distanceToPump/mpg <= fuelLeft
   };
+
+
+  // const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
