72527db51e299d3ba710cf0da8e5adbd74dbcf7a
Farangiz <109240720+Ergashgbaeva@users.noreply.github.com>
2022-11-09 19:23:59 +0500

    8kyu

--- script.js
+++ script.js
@@ -245,3 +245,8 @@ reverse = function(array) {
 
 
   // const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
+
+
+  function find_difference(a, b) {
+    return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
+  }
