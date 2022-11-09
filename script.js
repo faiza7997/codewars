0165515fe55fe188d625ccd28ebd8928fe71d627
Farangiz <109240720+Ergashgbaeva@users.noreply.github.com>
2022-11-09 19:24:57 +0500

    8kyu

--- script.js
+++ script.js
@@ -250,3 +250,9 @@ reverse = function(array) {
   function find_difference(a, b) {
     return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
   }
+
+
+
+  function find_difference(a, b) {
+    return Math.abs(a[0]*a[1]*a[2]-b[0]*b[1]*b[2]);
+  }
