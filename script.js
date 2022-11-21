92a461a2b37505521151b242f681643f00d13090
Farangiz <109240720+Ergashgbaeva@users.noreply.github.com>
2022-11-21 20:15:03 +0500

    8kyu

--- script.js
+++ script.js
@@ -361,3 +361,15 @@ function toCsvText(array) {
  }
  return res.trim();
 }
+
+
+
+function nextNumb(val) { 
+  while (val < 9876543210) {
+    val++;
+    if(val%2 && !(val%3) && val.toString().match(/^(?!.*(.).*\1)\d{1,10}$/)) {
+      return val;
+    }
+  }
+  return 'There is no possible number that fulfills those requirements';
+}
