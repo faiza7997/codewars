eb06a041e0411910c67560090f27f2a2227b6f22
Farangiz <109240720+Ergashgbaeva@users.noreply.github.com>
2022-11-28 23:31:54 +0500

    commit message

--- script.js
+++ script.js
@@ -414,3 +414,11 @@ function barTriang(p1, p2, p3){
   
   return [x, y];
 }
+
+
+function barTriang(p1, p2, p3){
+  let x = (p1[0] + p2[0] + p3[0]) / 3;
+  let y = (p1[1] + p2[1] + p3[1]) / 3;
+  
+  return [+x.toFixed(4), +y.toFixed(4)];
+}
