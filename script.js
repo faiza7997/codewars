9beca183d4f2ded08f1de1525e41078f0cff9ae6
Farangiz <109240720+Ergashgbaeva@users.noreply.github.com>
2022-11-30 20:43:25 +0500

    8kyu

--- script.js
+++ script.js
@@ -427,3 +427,14 @@ function barTriang(p1, p2, p3){
 
 const min = (list) => Math.min(...list);
 const max = (list) => Math.max(...list);
+
+
+var min = function(list){
+  list.sort((a, b) => (a - b));
+  return list[0];
+}
+
+var max = function(list){
+  list.sort((a, b) => (b - a));
+  return list[0];
+}
