04ae0f27b738a0d882fcc0491b29753ce80b0895
Farangiz <109240720+Ergashgbaeva@users.noreply.github.com>
2022-12-02 21:08:34 +0500

    8kyu

--- script.js
+++ script.js
@@ -518,3 +518,38 @@
 // }
 
 // console.log(saleHotdogs(9));
+
+// const ages = [32, 33, 16, 40];
+// const result = ages.filter(checkAdult);
+
+// function checkAdult(age) {
+//   return age >= 18;
+// }
+
+// console.log(result);
+
+
+
+
+
+const Arithmetics = {
+    add:(a, b) => {
+        return `${a} + ${b} = ${a+b}`;
+    },
+    subtract:(a, b) => {
+        return `${a} - ${b} = ${a-b}`
+    },
+    multiply:(a, b) => {
+        return `${a} * ${b} = ${a*b}`
+    },
+    division:(a, b) => {
+        if(b!=0) return `${a} / ${b} = ${a/b}`;
+        return `Cannot Divide by Zero!!!`;
+    }
+ 
+}
+ 
+console.log(Arithmetics.add(100, 100));
+console.log(Arithmetics.subtract(100, 7));
+console.log(Arithmetics.multiply(5, 5));
+console.log(Arithmetics.division(100, 5));
